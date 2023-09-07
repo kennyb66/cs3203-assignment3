f68bcf3c05ea77d9428e1e84e18fe1f9477d6527
kennyb66 <kenneth.e.bartel-1@ou.edu>
2023-09-07 00:30:27 -0500

    Added a method to compute the product of an array of ints

--- assignment3.java
+++ assignment3.java
@@ -1,10 +1,17 @@
 
 public class assignment3 {
-	public int sum(int[] arr) {
+	
+	public int sum(int[] sumArr) {
 		int sum = 0;
-		for(int i = 0; i < arr.length; i++) {
+		for(int i = 0; i < sumArr.length; i++) {
 			sum += arr[i];
 		}
 		return sum;
+	}
+	public int mul(int[] mulArr) {
+		int mul = 1;
+		for(int i = 0; i < mulArr.length; i++) {
+			mul = mul * arr[i];
+		}
 	}
 }
