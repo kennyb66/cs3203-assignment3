dedbbacd7c79e1cf5d96eb3b7caf3c96946f2a2e
kennyb66 <kenneth.e.bartel-1@ou.edu>
2023-09-07 01:42:12 -0500

    Added main method to take user input and print the result of the addition and multiplication functions

--- assignment3.java
+++ assignment3.java
@@ -1,17 +1,44 @@
+import java.util.Scanner;
 
-public class assignment3 {
-	
-	public int sum(int[] sumArr) {
+public class assignment3exe {
+	public static int sum(int[] sumArr) {
 		int sum = 0;
-		for(int i = 0; i < sumArr.length; i++) {
-			sum += arr[i];
+		for (int i = 0; i < sumArr.length; i++) {
+			sum += sumArr[i];
 		}
 		return sum;
 	}
-	public int mul(int[] mulArr) {
+
+	public static int mul(int[] mulArr) {
 		int mul = 1;
-		for(int i = 0; i < mulArr.length; i++) {
-			mul = mul * arr[i];
+		for (int i = 0; i < mulArr.length; i++) {
+			mul *= mulArr[i];
 		}
+		return mul;
+	}
+
+	public static void main(String[] args) {
+		int[] sumArr = new int[100];
+		int[] mulArr = new int[100];
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please input number of elements you need for addition calculation:");
+		int sumSize = keyboard.nextInt();
+		System.out.println("Please input your numbers to be computed for addition:");
+		for (int i = 0; i < sumSize; i++) {
+			sumArr[i] = keyboard.nextInt();
+		}
+		System.out.println("Result of addition: " + sum(sumArr));
+		System.out.println("Please input number of elements you need for multiplication calculation:");
+		int mulSize = keyboard.nextInt();
+		System.out.println("Please input your numbers to be computed for multiplication:");
+		for (int i = 0; i < mulSize; i++) {
+			mulArr[i] = keyboard.nextInt();
+		}
+		for (int j = mulSize; j < mulArr.length; j++) {
+			mulArr[j] = 1;
+		}
+		System.out.println("Result of multiplication: " + mul(mulArr));
+
+		keyboard.close();
 	}
 }
